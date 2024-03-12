a24d3e1e039d9f1dfaaa31c830bf6b36fc8d8314
Tran Ngoc Long <longth2707@gmail.com>
2024-03-12 23:19:03 +0700

    feature/CMP-53_View_all_magazines_in_faculty

---
+++ WebEnterprise_1640/Controllers/MagazineController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebEnterprise_1640.Models;
+using System.Collections.Generic;
+using System.Linq;
+using WebEnterprise_1640.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WebEnterprise_1640.Controllers
+{
+    public class MagazineController : Controller
+    {
+        private readonly ILogger<MagazineController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public MagazineController(ILogger<MagazineController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var magazines = await _dbContext.Magazines.ToListAsync();
+            var semesters = await _dbContext.Semesters.ToListAsync();
+
+            foreach (var magazine in magazines)
+            {
+                var semester = semesters.FirstOrDefault(s => s.Id == magazine.SemesterId);
+                if (semester != null)
+                {
+                    magazine.FinalDeadline = semester.FinalClosureDate;
+                }
+            }
+
+            return View(magazines);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
