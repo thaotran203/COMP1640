cd170f40a9cbb93afe6fafed64fcf5def3ed6eb2
Le Huyen Bao Trang <l.h.b.t.2k3@gmail.com>
2024-03-16 23:41:32 +0700

    Create Faculty controller and complete Create faculty function

---
+++ WebEnterprise_1640/Areas/Admin/Controllers/FacultyController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebEnterprise_1640.Data;
+using WebEnterprise_1640.Models;
+
+namespace WebEnterprise_1640.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FacultyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FacultyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Faculty
+        public async Task<IActionResult> Index()
+        {
+            return _context.Faculties != null ?
+                        View(await _context.Faculties.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Faculties'  is null.");
+        }
+
+        // GET: Admin/Faculty/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Faculty/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FacultyModel facultyModel)
+        {
+            _context.Add(facultyModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
