93289388608773ad2dc8343d2ae4e4baa1acc04c
Le Huyen Bao Trang <l.h.b.t.2k3@gmail.com>
2024-03-24 00:14:32 +0700

    Authorize

--- WebEnterprise_1640/Areas/Admin/Controllers/FacultyController.cs
+++ WebEnterprise_1640/Areas/Admin/Controllers/FacultyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using WebEnterprise_1640.Models;
 namespace WebEnterprise_1640.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class FacultyController : Controller
     {
         private readonly ApplicationDbContext _context;
