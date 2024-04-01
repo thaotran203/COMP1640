8a4ed8494cbd3734a2ee4dcd63dfa81fc7816ad4
Le Huyen Bao Trang <l.h.b.t.2k3@gmail.com>
2024-04-01 14:49:28 +0700

    Fix model

--- WebEnterprise_1640/Models/FacultyModel.cs
+++ WebEnterprise_1640/Models/FacultyModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebEnterprise_1640.Models
@@ -11,13 +12,11 @@ namespace WebEnterprise_1640.Models
 
         [Required]
         public string Name { get; set; }
-
-        [Required]
         public int CoordinatorId { get; set; }
-
+        [ValidateNever]
         [NotMapped]
         public List<UserModel> Users { get; set; }
-
+        [ValidateNever]
         [NotMapped]
         public List<MagazineModel> Magazines { get; set; }
     }
