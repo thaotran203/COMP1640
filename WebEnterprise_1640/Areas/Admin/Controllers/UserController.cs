7fa0899eb161cc6de6c80878264aab9a3a556430
Le Huyen Bao Trang <l.h.b.t.2k3@gmail.com>
2024-04-25 07:17:09 +0700

    Fix UI user

--- WebEnterprise_1640/Areas/Admin/Controllers/UserController.cs
+++ WebEnterprise_1640/Areas/Admin/Controllers/UserController.cs
@@ -10,6 +10,7 @@ using WebEnterprise_1640.Models;
 using WebEnterprise_1640.Models.ViewModel;
 using Microsoft.EntityFrameworkCore;
 using WebEnterprise_1640.Models.NewFolder;
+using System.Text.Json;
 
 namespace WebEnterprise_1640.Areas.Admin.Controllers
 {
@@ -51,8 +52,34 @@ namespace WebEnterprise_1640.Areas.Admin.Controllers
         }
         public async Task<IActionResult> Index(string search = "", List<string> roles = null, List<int> facultyIds = null)
         {
-            //Get all users first
-            var users = await _userManager.Users.ToListAsync();
+			var userJson = HttpContext.Session.GetString("USER");
+			UserModel userM = null;
+			if (userJson != null && userJson.Length > 0)
+			{
+				userM = JsonSerializer.Deserialize<UserModel>(userJson);
+			}
+			if (userM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var userRole = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userM.Id);
+			if (userRole == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var roleM = _context.Roles.FirstOrDefault(r => r.Id == userRole.RoleId);
+			if (roleM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			if (roleM.Name.ToLower() != "admin")
+			{
+				return Redirect("/Account/Login");
+			}
+			ViewBag.User = userM;
+
+			//Get all users first
+			var users = await _userManager.Users.ToListAsync();
             //Filter out users with the 'Admin' role
             users = users.Where(u => !_userManager.IsInRoleAsync(u, "Admin").Result).Where(u => !_userManager.IsInRoleAsync(u, "Guest").Result).ToList();
             //Search
@@ -89,7 +116,33 @@ namespace WebEnterprise_1640.Areas.Admin.Controllers
         // GET: Admin/User/Register
         public async Task<IActionResult> Register()
         {
-            if (!_context.Roles.Any())
+			var userJson = HttpContext.Session.GetString("USER");
+			UserModel userM = null;
+			if (userJson != null && userJson.Length > 0)
+			{
+				userM = JsonSerializer.Deserialize<UserModel>(userJson);
+			}
+			if (userM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var userRole = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userM.Id);
+			if (userRole == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var roleM = _context.Roles.FirstOrDefault(r => r.Id == userRole.RoleId);
+			if (roleM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			if (roleM.Name.ToLower() != "admin")
+			{
+				return Redirect("/Account/Login");
+			}
+			ViewBag.User = userM;
+
+			if (!_context.Roles.Any())
             {
                 await _roleManager.CreateAsync(new IdentityRole { Id = "1", Name = "Guest" });
                 await _roleManager.CreateAsync(new IdentityRole { Id = "2", Name = "Student" });
@@ -254,7 +307,33 @@ namespace WebEnterprise_1640.Areas.Admin.Controllers
         // GET: Admin/User/Edit/5
         public async Task<IActionResult> Edit(string? id)
         {
-            if (id == null)
+			var userJson = HttpContext.Session.GetString("USER");
+			UserModel userM = null;
+			if (userJson != null && userJson.Length > 0)
+			{
+				userM = JsonSerializer.Deserialize<UserModel>(userJson);
+			}
+			if (userM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var userRole = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userM.Id);
+			if (userRole == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var roleM = _context.Roles.FirstOrDefault(r => r.Id == userRole.RoleId);
+			if (roleM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			if (roleM.Name.ToLower() != "admin")
+			{
+				return Redirect("/Account/Login");
+			}
+			ViewBag.User = userM;
+
+			if (id == null)
             {
                 return NotFound();
             }
@@ -396,7 +475,33 @@ namespace WebEnterprise_1640.Areas.Admin.Controllers
         // GET: Admin/User/Delete/5
         public async Task<IActionResult> Delete(string? id)
         {
-            if (id == null || id == null)
+			var userJson = HttpContext.Session.GetString("USER");
+			UserModel userM = null;
+			if (userJson != null && userJson.Length > 0)
+			{
+				userM = JsonSerializer.Deserialize<UserModel>(userJson);
+			}
+			if (userM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var userRole = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userM.Id);
+			if (userRole == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			var roleM = _context.Roles.FirstOrDefault(r => r.Id == userRole.RoleId);
+			if (roleM == null)
+			{
+				return Redirect("/Account/Login");
+			}
+			if (roleM.Name.ToLower() != "admin")
+			{
+				return Redirect("/Account/Login");
+			}
+			ViewBag.User = userM;
+
+			if (id == null || id == null)
             {
                 return NotFound();
             }
